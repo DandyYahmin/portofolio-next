288c8f639525d76899a83a59c0a941acc4c5ea77
unknown <dandydarmawanalyahmin@gmail.com>
2023-10-29 20:10:55 +0700

    image

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
-
+    images: {
+        loader: "akamai",
+        path: "",
+    },
     // Add basePath
     basePath: '/portofolio-next',
     assetPrefix: '/portofolio-next'
