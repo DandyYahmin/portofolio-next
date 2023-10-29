97d8717fda09e1bc874a0a2d86ed33f407a3b323
unknown <dandydarmawanalyahmin@gmail.com>
2023-10-29 20:19:04 +0700

    bismillah

--- src/app/components/Article.jsx
+++ src/app/components/Article.jsx
@@ -5,7 +5,7 @@ const Article = () => {
   return (
     <div className='grid grid-cols-1'>
         <div className="place-self-center">
-            <Image slt="img" src="/images/Coding-bro.png" width={400} height={400}/>
+            <Image alt="img" src="/images/Coding-bro.png" width={400} height={400}/>
             <h1 className='text-[#92E3A9] mt-2 mb-20 text-4xl lg:text-6xl font-extrabold text-center'>Coming Soon</h1>
         </div>
         
